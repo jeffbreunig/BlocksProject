03bc9cfc387c928a962dee568629644f253486cf
jeffbreunig <jeffkbreunig>
2012-07-08 14:41:48 -0500

    Initial Commit

---
+++ BlocksProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlocksProject
+//
+//  Created by Jeff Breunig on 7/8/12.
+//  Copyright (c) 2012 Easy Touch, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
